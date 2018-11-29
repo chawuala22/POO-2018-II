d3f9a2248626cb0278db5cde4961c651211a1465
Miguel Rojas <mrojasgarcia22@gmail.com>
2018-11-29 14:14:27 -0500

    Mejoramiento de vista, falta llenar con JSON

--- app/src/main/java/com/example/parkingapp/unimagadalena/parkingapp/MapsActivity.java
+++ app/src/main/java/com/example/parkingapp/unimagadalena/parkingapp/MapsActivity.java
@@ -26,7 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_informacion);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapfirst);
+                .findFragmentById(R.id.map1);
         mapFragment.getMapAsync(this);
 
 
