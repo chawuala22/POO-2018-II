df997bf4982a84e617272aae29c10357505618ab
Miguel Rojas <mrojasgarcia22@gmail.com>
2018-12-02 23:42:11 -0500

    Solo falta la img

--- app/src/main/java/com/example/parkingapp/unimagadalena/parkingapp/Informacion.java
+++ app/src/main/java/com/example/parkingapp/unimagadalena/parkingapp/Informacion.java
@@ -21,6 +21,80 @@ import java.util.ArrayList;
 
 public class Informacion extends AppCompatActivity {
 
+    TextView nombre;
+    TextView direccion;
+    TextView propietario;
+    TextView costo;
+    TextView horario;
+    TextView cupo;
+    ImageView imageView;
+
+    String name;
+    String dire;
+    String propie;
+    String costor;
+    String hori;
+    String cupito;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion);
+
+        nombre=findViewById(R.id.nombre);
+        direccion= findViewById(R.id.direccion);
+        propietario=findViewById(R.id.propietario);
+        costo=findViewById(R.id.costo);
+        horario=findViewById(R.id.horario);
+        cupo=findViewById(R.id.cupo);
+        imageView=findViewById(R.id.imageView);
+
+        name = getIntent().getStringExtra("name");
+        dire=getIntent().getStringExtra("dire");
+        propie=getIntent().getStringExtra("propi");
+        costor=getIntent().getStringExtra("costo");
+        hori=getIntent().getStringExtra("horar");
+        cupito=getIntent().getStringExtra("cup");
+
+
+
+        nombre.setText(name);
+        direccion.setText(dire);
+        propietario.setText(propie);
+        costo.setText(costor);
+        horario.setText(hori);
+        cupo.setText(cupito);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
+
+/*package com.example.parkingapp.unimagadalena.parkingapp;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Informacion extends AppCompatActivity {
+
    TextView nombre;
    TextView direccion;
    TextView propietario;
@@ -83,3 +157,4 @@ public class Informacion extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
+*/
--- app/src/main/java/com/example/parkingapp/unimagadalena/parkingapp/Lista.java
+++ app/src/main/java/com/example/parkingapp/unimagadalena/parkingapp/Lista.java
@@ -6,6 +6,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.DataSetObserver;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -46,6 +47,8 @@ public class Lista extends Fragment  {
     ListView listView;
 
     View v;
+    ArrayList<Product> products = new ArrayList<>();
+
 
     public Lista() {
         // Required empty public constructor
@@ -53,46 +56,162 @@ public class Lista extends Fragment  {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_lista, container, false);
 
         listView = (ListView)v.findViewById(R.id.listViewLista);
 
-        ArrayList<Product> products = new ArrayList<>();
+        int m = R.drawable.park1;
 
+        products.add(new Product(1,"El Tesoro", "Cra. 21c #29A-38", "Ernesto Campo","22.000",
+                "8:00 am - 11:00 pm","70",m));
 
-        products.add(new Product("El Tesoro", "Cra. 21c #29A-38", R.drawable.park1));
-        products.add(new Product("La Guaca", "Cra. 22 #33a-2 a 33a-34", R.drawable.guaca));
-        products.add(new Product("Park Center", "Cra. 29 #29j-10 a 29j-52", R.drawable.park));
-        products.add(new Product("Cubriseal", "Cra. 2 #16-2 a 16-76", R.drawable.cubriseal));
-        products.add(new Product("Paso Nivel", "Cra. 19b #26a-2 a 26a-74", R.drawable.pasonivel));
-        products.add(new Product("Pavimark", "Cl. 17 #7-161 a 7-1", R.drawable.pavimark));
-        products.add(new Product("El Cartel", "Cl. 23 #6-2 a 6-122", R.drawable.cartel));
-        products.add(new Product("Socoda", "Cl. 29L1 #54", R.drawable.pasonivel));
+        products.add(new Product(2,"La Guaca", "Cra. 22 #33a-2 a 33a-34","Brian Calsadilla","11.000",
+                "10:00 am - 11:00 pm","40",R.drawable.guaca));
+
+        products.add(new Product(3,"Park Center", "Cra. 29 #29j-10 a 29j-52","Luis Carlos Galán","15.000",
+                "24 horas","15",R.drawable.park));
+
+        products.add(new Product(4,"Cubriseal", "Cra. 2 #16-2 a 16-76","Cristian Lopez","14.000",
+                "24 Horas","30",R.drawable.cubriseal));
+
+        products.add(new Product(5,"Paso Nivel", "Cra. 19b #26a-2 a 26a-74","Jorge Romero","14.000",
+                "8:00 am - 10:00 pm","20",R.drawable.pasonivel));
+
+        products.add(new Product(6,"Pavimark", "Cl. 17 #7-161 a 7-1","Oscar Leon","17.000",
+                "6:00 am - 10:00 pm","25",R.drawable.pavimark));
+
+        products.add(new Product(7,"El Cartel", "Cl. 23 #6-2 a 6-122","Gilma Lopez","20.000",
+                "24 horas","25",R.drawable.cartel));
+
+        products.add(new Product(8,"Socoda", "Cl. 29L1 #54","Omar Cantillo","15.000",
+                "24 horas","40",R.drawable.pasonivel));
 
         listView.setAdapter(new ListProductAdapter(getContext(), products));
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+                String name;
+                String diree;
+                String propi;
+                String costo;
+                String hora;
+                String cupo;
 
-                        Intent myIntent = new Intent(getContext(), Informacion.class);
-                        startActivity(myIntent);
 
-               
+                for(Product list : products){
+                    if(list.getId()-1 == position) {
+                        name = list.getName();
+                        diree=list.getDireccion();
+                        propi=list.getPropietario();
+                        costo=list.getCostar();
+                        hora=list.getHora();
+                        cupo=list.getCup();
+
+                        Intent intent = new Intent(getContext(), Informacion.class);
+
+                        intent.putExtra("name", name);
+                        intent.putExtra("dire", diree);
+                        intent.putExtra("propi", propi);
+                        intent.putExtra("costo", costo);
+                        intent.putExtra("horar", hora);
+                        intent.putExtra("cup", cupo);
+
+
+                        startActivity(intent);
+                    }
+                }
             }
         });
         return v;
-        }
+    }
 
     public class Product {
+        int id;
         String name;
         String direccion;
+        String propietario;
+        String costar;
+        String Hora;
+        String cup;
+
         int image;
 
-        public Product(String name, String price, int image) {
+        public Product(int id, String name, String direccion,String propietario, String costar, String Hora,String cup, int image) {
+            this.id = id;
             this.name = name;
             this.direccion = direccion;
+            this.costar=costar;
+            this.image = image;
+            this.Hora=Hora;
+            this.cup=cup;
+            this.propietario=propietario;
+        }
+
+
+
+        public String getCup() {
+            return cup;
+        }
+
+        public void setCup(String cup) {
+            this.cup = cup;
+        }
+
+        public String getHora() {
+            return Hora;
+        }
+
+        public void setHora(String hora) {
+            Hora = hora;
+        }
+
+        public String getCostar() {
+            return costar;
+        }
+
+        public void setCostar(String costar) {
+            this.costar = costar;
+        }
+
+        public String getPropietario() {
+            return propietario;
+        }
+
+        public void setPropietario(String propietario) {
+            this.propietario= propietario;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDireccion() {
+            return direccion;
+        }
+
+        public void setDireccion(String direccion) {
+            this.direccion = direccion;
+        }
+
+        public int getImage() {
+            return image;
+        }
+
+        public void setImage(int image) {
             this.image = image;
         }
     }
@@ -155,6 +274,3 @@ public class Lista extends Fragment  {
         }
     }
 }
-
-
-
